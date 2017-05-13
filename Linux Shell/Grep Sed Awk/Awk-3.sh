8f690ec28fc14266b6132d1cbe37c25080e937ad
zabit2 <zabit.rahman2@gmail.com>
2017-05-13 19:17:46 +0530

    print grades based on conditions

---
+++ Linux Shell/Grep Sed Awk/Awk-3.sh
@@ -0,0 +1,12 @@
+awk '
+{
+total=($2+$3+$4)/3;
+if(total>80)
+print $1,$2,$3,$4,": A";
+else if(total>=60 && total<80)
+print $1,$2,$3,$4,": B";
+else if(total>=50 && total<60)
+print $1,$2,$3,$4,": C";
+else
+print $1,$2,$3,$4,": FAIL";
+}'
