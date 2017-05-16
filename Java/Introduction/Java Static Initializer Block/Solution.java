b269f467d42184e791631f5a2b5db0a65fa627be
zabit2 <zabit.rahman2@gmail.com>
2017-05-16 22:34:17 +0530

    use static initialisers

---
+++ Java/Introduction/Java Static Initializer Block/Solution.java
@@ -0,0 +1,22 @@
+static Scanner sc = new Scanner(System.in);
+static int H = sc.nextInt();
+static int B = sc.nextInt();
+static boolean flag = true;
+
+static
+{
+    try
+    {
+        if(B<=0 || H<=0)
+        {
+            flag=false;
+            throw new Exception("Breadth and height must be positive");    
+        }
+            
+    }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    
+}
