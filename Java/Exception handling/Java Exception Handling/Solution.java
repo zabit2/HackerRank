b8a8c9c87c7507fa15250275a7d59ac85f981c80
zabit2 <zabit.rahman2@gmail.com>
2017-05-18 00:10:19 +0530

    throws keyword use

---
+++ Java/Exception handling/Java Exception Handling/Solution.java
@@ -0,0 +1,12 @@
+//Write your code here
+import java.lang.Math;
+class MyCalculator
+{
+    public int power(int n,int p) throws Exception
+    {
+        if(n<0 || p<0)
+                throw new Exception("n and p should be non-negative");
+         return (int)Math.pow(n,p);  
+        
+    }
+}
